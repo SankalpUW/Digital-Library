0b12be5ff89f8b1aa87320f1b19ff8a1b8410ab4
SankalpUW <sankalpdevaiah2004@gmail.com>
2024-09-08 13:01:25 -0400

    fixed spring config test error

--- book-network/src/test/java/com/sankalpdk/book_network/BookNetworkApplicationTests.java
+++ book-network/src/test/java/com/sankalpdk/book_network/BookNetworkApplicationTests.java
@@ -3,7 +3,9 @@ package com.sankalpdk.book_network;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.sankalpdk.book.BookNetworkApiApplication;
+
+@SpringBootTest(classes = BookNetworkApiApplication.class)
 class BookNetworkApplicationTests {
 
 	@Test
